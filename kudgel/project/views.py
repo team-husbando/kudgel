d717d5c0c7bc1bef49dc55a6df9f55a8dab95615
David Stewart <davidstewy@gmail.com>
2019-04-04 14:46:33 -0400

    added role form, role form view

--- kudgel/project/views.py
+++ kudgel/project/views.py
@@ -1,12 +1,23 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from rest_framework import viewsets
-from kudgel.project.models import Project
+from kudgel.project.models import Project, Role
 from kudgel.project.serializers import ProjectSerializer
+from .forms import RoleForm
 
-# Create your views here.
+
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
 
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class RoleFormView(LoginRequiredMixin, CreateView):
+    model = Role
+    template_url = 'generic.form.html'
+    form_class = RoleForm
+    sucess_url = reverse_lazy('success')
