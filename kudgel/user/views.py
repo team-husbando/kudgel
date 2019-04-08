a96880d70341a592f189497d4b39aad1c1773bb8
David Stewart <davidstewy@gmail.com>
2019-04-08 16:05:14 -0400

    added shift_detail views and user_detail views

--- kudgel/user/views.py
+++ kudgel/user/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
@@ -25,3 +26,10 @@ class CreateUserView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'generic/form.html'
+
+class UserDetailView(DetailView):
+
+    model = User
+
+    def get_object(self, queryset=None):
+        return User.objects.get(id=self.kwargs.get('slug'))
